c4ef37a273e7ee5868320af8e28df7fcd3c2d928
aemzayn <ahmad.muslih22@gmail.com>
2021-08-04 15:55:38 +0700

    remove preact

--- next.config.js
+++ next.config.js
@@ -3,17 +3,17 @@ module.exports = {
     strictPostcssConfiguration: true
   },
 
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-    // https://github.com/leerob/leerob.io/blob/main/next.config.js#L26-L37
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat'
-      })
-    }
+  // webpack: (config, { dev, isServer }) => {
+  //   // Replace React with Preact only in client production build
+  //   // https://github.com/leerob/leerob.io/blob/main/next.config.js#L26-L37
+  //   if (!dev && !isServer) {
+  //     Object.assign(config.resolve.alias, {
+  //       react: 'preact/compat',
+  //       'react-dom/test-utils': 'preact/test-utils',
+  //       'react-dom': 'preact/compat'
+  //     })
+  //   }
 
-    return config
-  }
+  //   return config
+  // }
 }
